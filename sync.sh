26b2c307fcda05ddc6dc5c9db47a51c83c0b9d0a
Yuyang Liu <lhtlyy@gmail.com>
2012-09-23 22:49:57 +0800

    竟然md语法链接内的_都要escape

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 python2 ~/sagent &
 
 git add .
-git commit -m "blog update"
+git commit -m "竟然md语法链接内的_都要escape"
 tsocks git push publish master
 tsocks git push master
 ## Maybe I should add to top in blog.
