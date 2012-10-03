0b908692232811ee158cfae1a49da90512e00f16
Yuyang Liu <lhtlyy@gmail.com>
2012-10-03 19:10:07 +0800

    隐藏cnzz

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 python2 ~/sagent &
 
 git add .
-git commit -m "竟然md语法链接内的_都要escape"
+git commit -m "隐藏cnzz"
 tsocks git push publish master
 tsocks git push master
 ## Maybe I should add to top in blog.
