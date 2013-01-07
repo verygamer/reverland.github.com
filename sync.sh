21b8cec9a12af9a0e7f3b767ad0e8260a8eaa0e8
Yuyang Liu <lhtlyy@gmail.com>
2013-01-07 22:55:17 +0800

    blog update

--- sync.sh
+++ sync.sh
@@ -4,7 +4,7 @@ git add .
 git commit -m "blog update"
 echo "pushing to github"
 git push master master # github
-echo"github pushed"
+echo "github pushed"
 echo "pushing to phoenixzsec"
 git push publish master # @phoenixzsec
 echo "phoenixzsec pushed"
